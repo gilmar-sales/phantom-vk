cfb869c8aada8877fe49d44627c72614fda53d7e
gilmarxd <gilmar_custodio_@hotmail.com>
2022-01-19 13:13:22 -0300

    feat: configure git and cmake

---
+++ phpch.h
@@ -0,0 +1,13 @@
+//
+// Created by gilmar on 1/19/22.
+//
+
+#ifndef PHANTOMVK_PHPCH_H
+#define PHANTOMVK_PHPCH_H
+
+#include <string>
+#include <memory>
+#include <algorithm>
+#include <vector>
+
+#endif //PHANTOMVK_PHPCH_H
